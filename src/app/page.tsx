93e504e401284fa89be2073f3a815c6be9a3f2fa
Nguyễn Tuấn Vinh <59060940+Vinh1222001@users.noreply.github.com>
2023-11-07 18:48:42 +0700

    redirecting to ggdrive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,11 @@
 import React from "react";
 
 import WebsiteUnderConstruction from "@/components/WebsiteUnderConstruction";
+import { redirect } from "next/navigation";
 // import { AiFillInstagram } from "react-icons/ai";
 
 export default function Home() {
-
+  redirect("https://drive.google.com/drive/folders/14dnzkBAEB0tVMoJzzdY4wvrS_mRW2c3h?usp=sharing")
   return (
     <main className="flex min-h-screen flex-col justify-between text-white">
       <WebsiteUnderConstruction/>
